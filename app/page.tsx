4bf2114b6137ca5ba65f3ebd6dcecfb69ff897b0
Adnan Khan <adnanonline109@gmail.com>
2024-10-11 21:15:23 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Countdown from "@/components/count-down"
+
+
+export default function Home(){
+  return(
+    <div>
+      <Countdown />
+      <h1>Made By Adnan Khan</h1>
+    </div>
+  );
+}
